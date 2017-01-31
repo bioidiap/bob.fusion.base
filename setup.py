c9071aecec951d4ec31a8710f5985ff26c993720
Amir Mohammadi <183.amir@gmail.com>
2017-01-31 17:24:03 +0100

    update setup.py file

--- setup.py
+++ setup.py
@@ -1,37 +1,5 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
-# Andre Anjos <andre.anjos@idiap.ch>
-# Mon 16 Apr 08:18:08 2012 CEST
-#
-# Copyright (C) Idiap Research Institute, Martigny, Switzerland
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-# This file contains the python (distutils/setuptools) instructions so your
-# package can be installed on **any** host system. It defines some basic
-# information like the package name for instance, or its homepage.
-#
-# It also defines which other packages this python package depends on and that
-# are required for this package's operation. The python subsystem will make
-# sure all dependent packages are installed or will install them for you upon
-# the installation of this package.
-#
-# The 'buildout' system we use here will go further and wrap this package in
-# such a way to create an isolated python working environment. Buildout will
-# make sure that dependencies which are not yet installed do get installed, but
-# **without** requiring administrative privileges on the host system. This
-# allows you to test your package with new python dependencies w/o requiring
-# administrative interventions.
 
 from setuptools import setup, dist
 dist.Distribution(dict(setup_requires=['bob.extension']))
@@ -47,13 +15,13 @@ setup(
     # information before releasing code publicly.
     name='bob.fusion.base',
     version=open("version.txt").read().rstrip(),
-    description='Basic tools for running score fusion experiments',
+    description='Tools for running score fusion in biometric experiments',
 
-    url='https://www.github.com/bioidiap/bob.fusion.base',
+    url='https://gitlab.idiap.ch/bob/bob.fusion.base',
     license='GPLv3',
     author='Amir Mohammadi',
     author_email='amir.mohammadi@idiap.ch',
-    keywords='bob, score fusion, evaluation',
+    keywords='bob, score fusion, evaluation, biometric',
 
     # If you have a better, long description of your package, place it on the
     # 'doc' directory and then hook it here
@@ -111,6 +79,7 @@ setup(
         'llr-skl     = bob.fusion.base.config.algorithm.llr_skl:algorithm',
         'plr-2       = bob.fusion.base.config.algorithm.plr_2:algorithm',
         'mlp         = bob.fusion.base.config.algorithm.mlp:algorithm',
+        'gmm         = bob.fusion.base.config.algorithm.gmm:algorithm',
       ],
 
         },
