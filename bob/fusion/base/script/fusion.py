1cf166fc6ccf2636b1cb4b6cac80d1f5376c0145
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-12-11 12:03:57 +0100

    use aliased group in click commands

--- bob/fusion/base/script/fusion.py
+++ bob/fusion/base/script/fusion.py
@@ -3,10 +3,11 @@
 import click
 import pkg_resources
 from click_plugins import with_plugins
+from bob.extension.scripts.click_helper import AliasedGroup
 
 
 @with_plugins(pkg_resources.iter_entry_points('bob.fusion.cli'))
-@click.group()
+@click.group(cls=AliasedGroup)
 def fusion():
     """Score fusion related scripts"""
     pass
